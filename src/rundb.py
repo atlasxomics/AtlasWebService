684bd1e72a07de9bfd5e9f5af98dce694406a2ee
Jonahs11 <jonahsilverman11@gmail.com>
2022-09-06 11:18:04 -0400

    More dynamic getColumns function

--- src/rundb.py
+++ src/rundb.py
@@ -23,7 +23,7 @@ class MariaDB:
     
     def initialize(self):
         try:
-            self.client = mysql.connector.connect(user=self.username, password=self.password,  host=self.host, port=self.port)
+            self.client = mysql.connector.connect(user=self.username, password=self.password,  host=self.host, port=self.port, database = self.db)
             self.cursor = self.client.cursor()
             print(self.client)
         except Exception as e:
@@ -33,24 +33,49 @@ class MariaDB:
     def initEndpoints(self):
 
         @self.auth.app.route('/api/v1/run_db/get_columns_runid', methods=['GET'])
-        @self.auth.login_required
-        def _getColumns(self, methods=['GET']):
+        def _getColumns():
             print('wahoo')
-            run_id = request.args.get('run_id', type=str)
+            run_ids = json.loads(request.args.get('run_ids', default=[]))
+            columns = json.loads(request.args.get('columns', default=[]))
+            table = request.args.get('table', default="dbit_metadata", type=str)
             status_code = 200
+            print(run_ids)
+            print(columns)
             try:
-                sql = """SELECT * FROM dbit_metadata
-                        WHERE cntn_cf_runId = run_id
-                    """
-                res = self.cursor.execute(sql)
-                print(res)
+                res = self.getColumns(run_ids, columns, table)
             except Exception as e:
                 status_code = 500
                 exc = traceback.format_exc()
                 res = utils.error_message("{} {}".format(str(e), exc))
             finally:
-                resp = Response(data = json.dumps(res), status=status_code)
+                resp = Response(json.dumps(res), status=status_code)
                 return resp
+
+    def getColumns(self, run_ids, columns, table):
+        sql1 = "SELECT "
+        if len(columns) > 0:
+            for i in range(len(columns)):
+                if i != len(columns) - 1:
+                    add = columns[i] + ", "
+                else:
+                    add = columns[i]
+                sql1 += add
+        else:
+            sql1 += "*"
+
+        sql2 = " FROM {}".format(table)
+        if len(run_ids) > 0:
+            tup = tuple(run_ids)
+            sql3 = " WHERE cntn_cf_runId in {};".format(tup)
+        else:
+            sql3 = ";"
+        sql = sql1 + sql2 + sql3
+        print(sql)
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        print(result)
+        return result
+
 
 
 
